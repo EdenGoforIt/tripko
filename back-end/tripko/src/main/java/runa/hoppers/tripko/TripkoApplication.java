215a55a6ddfcf78925767aeb95bce21e51c67fa7
sunny <parks38@uw.edu>
2021-03-02 18:59:34 +0900

    feat : init backend

---
+++ back-end/tripko/src/main/java/runa/hoppers/tripko/TripkoApplication.java
@@ -0,0 +1,13 @@
+package runa.hoppers.tripko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripkoApplication.class, args);
+    }
+
+}
---
+++ back-end/tripko/src/test/java/runa/hoppers/tripko/TripkoApplicationTests.java
@@ -0,0 +1,13 @@
+package runa.hoppers.tripko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
